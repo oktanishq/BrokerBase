3ecb4ae7e3e6297a1e018b87db9ac73a6a596526
Tanishq Khatri <ktanishq991@gmail.com>
2025-12-31 15:31:09 +0530

    COnnect homepage listing to database

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,7 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\SettingsController;
-use App\Http\Controllers\Admin\PropertyController;
+use App\Http\Controllers\Admin\PropertyController as AdminPropertyController;
+use App\Http\Controllers\PropertyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,25 +27,29 @@ Route::get('/settings', [SettingsController::class, 'getSettings']);
 // Property Management API routes (Web Form Authentication)
 Route::middleware(['auth', 'web'])->group(function () {
     // Property CRUD operations
-    Route::get('/admin/properties', [PropertyController::class, 'index']);
-    Route::post('/admin/properties', [PropertyController::class, 'store']);
-    Route::get('/admin/properties/{property}', [PropertyController::class, 'show']);
-    Route::match(['put', 'patch'], '/admin/properties/{property}', [PropertyController::class, 'update']);
-    Route::delete('/admin/properties/{property}', [PropertyController::class, 'destroy']);
+    Route::get('/admin/properties', [AdminPropertyController::class, 'index']);
+    Route::post('/admin/properties', [AdminPropertyController::class, 'store']);
+    Route::get('/admin/properties/{property}', [AdminPropertyController::class, 'show']);
+    Route::match(['put', 'patch'], '/admin/properties/{property}', [AdminPropertyController::class, 'update']);
+    Route::delete('/admin/properties/{property}', [AdminPropertyController::class, 'destroy']);
     
     // Draft operations
-    Route::post('/admin/properties/draft', [PropertyController::class, 'storeDraft']);
+    Route::post('/admin/properties/draft', [AdminPropertyController::class, 'storeDraft']);
     
     // Status management
-    Route::put('/admin/properties/{property}/status', [PropertyController::class, 'updateStatus']);
+    Route::put('/admin/properties/{property}/status', [AdminPropertyController::class, 'updateStatus']);
 });
 
 // Property Management API routes (SPA/Mobile Authentication)
 Route::middleware('auth:sanctum')->group(function () {
     // Property CRUD operations for API clients
-    Route::get('/admin/properties-spa', [PropertyController::class, 'index']);
-    Route::post('/admin/properties-spa', [PropertyController::class, 'store']);
-    Route::get('/admin/properties-spa/{property}', [PropertyController::class, 'show']);
-    Route::match(['put', 'patch'], '/admin/properties-spa/{property}', [PropertyController::class, 'update']);
-    Route::delete('/admin/properties-spa/{property}', [PropertyController::class, 'destroy']);
+    Route::get('/admin/properties-spa', [AdminPropertyController::class, 'index']);
+    Route::post('/admin/properties-spa', [AdminPropertyController::class, 'store']);
+    Route::get('/admin/properties-spa/{property}', [AdminPropertyController::class, 'show']);
+    Route::match(['put', 'patch'], '/admin/properties-spa/{property}', [AdminPropertyController::class, 'update']);
+    Route::delete('/admin/properties-spa/{property}', [AdminPropertyController::class, 'destroy']);
 });
+
+// Public Property API routes (no authentication required)
+Route::get('/properties', [PropertyController::class, 'getProperties']);
+Route::get('/properties/{id}', [PropertyController::class, 'getProperty']);
