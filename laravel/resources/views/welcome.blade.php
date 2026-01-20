d003c07cb2b6b4308fc297cbd098def01853200d
Tanishq Khatri <ktanishq991@gmail.com>
2026-01-20 15:58:47 +0530

    Homepage converted

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
 @vite('resources/css/app.css')
 <script src="{{ asset('js/alpine-components.js') }}"></script>
-<script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
 <style>
 .no-scrollbar::-webkit-scrollbar {
     display: none;
